20e2bd7430744567a58f055352536e9156cf88c2
rovedajr <contato@rovedajr.com>
2020-07-22 21:59:47 -0300

    Damn…

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 const express = require('express')
 const app = express()
 const porta = 3333
+const projects = []
 
-app.get('/projects', (req, res, next) => {
-    return res.json(['Projeto 1', 'Projeto 2'])
+app.use(express.json())
+
+app.get('/projects', (req, res) => {
+    // const { title, owner } = req.query
+
+    // console.log(title)
+    // console.log(owner)
+    // console.log(req.query);
+
+    return res.json(projects)
 })
 
 app.post('/projects', (req, res, next) => {
-    return res.json(['Projeto 1', 'Projeto 2', 'Projeto 3'])
+    const { title, owner } = req.body
+
+    const projects = { title, owner }
+
+    return res.json()
 })
 
 app.put('/projects/:id', (req, res, next) => {
+    const { id } = req.params
+    console.log(id);
+
     return res.json(['Projeto 4', 'Projeto 2', 'Projeto 3'])
 })
 
